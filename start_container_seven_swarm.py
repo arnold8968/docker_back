e6270253cf59e5b714a40e844b3ce2faf3f70d5b
James Yan <840520333@qq.com>
2020-02-26 22:01:41 -0500

    Update start_container_seven_swarm.py

--- start_container_seven_swarm.py
+++ start_container_seven_swarm.py
@@ -14,15 +14,15 @@ time.sleep(5)
 
 def run_container(container_name,container_model):
     #command_run = 'nohup docker run --name {} {} > {}.log &'.format(container_name,container_model,container_name)
-    command_run =  "docker service create -d --restart-condition none --name {} {} > {}.log &". format(container_name,container_model,container_name)
+    command_run =  "nohup docker service create -d --restart-condition none --name {} {} > {}.log &". format(container_name,container_model,container_name)
     subprocess.Popen(command_run,shell=True)
     print("Succesfully run container ",container_name,"collecting data now!")
 
 container_list = ["test1","test2","test3","test4","test5","test6","test7"]
 container_model_list = ["fuzzychen/1000batch","fuzzychen/vgg16","fuzzychen/inceptionv3","fuzzychen/res50","fuzzychen/xcep", "fuzzychen/inceptionv3","fuzzychen/res50"]
 
-for i in range(len(container_list)):
-    run_container(container_list[i], random.choice(container_model_list))
+for i in range(1, 29):
+    run_container('test{}'.format(i), random.choice(container_model_list))
     print("initializing~~~~~~~~~~~~Takes 180 seconds")
 
 time.sleep(180)
