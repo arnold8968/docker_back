fbcabf1c1803ab9c26b2f943588a47c83ba51853
James Yan <840520333@qq.com>
2020-02-28 14:30:43 -0500

    Update start_container_seven.py

--- start_container_seven.py
+++ start_container_seven.py
@@ -23,8 +23,8 @@ ran_list = random.sample(range(len(container_model_list)), len(container_model_l
 
 for i in range(len(container_list)):
     run_container(container_list[i], container_model_list[ran_list[i % len(container_model_list)]])
-    print(container_list[i], container_model_list[i % len(container_model_list)])
+    print(container_list[i], container_model_list[ran_list[i % len(container_model_list)]])
     print("initializing~~~~~~~~~~~~Takes 180 seconds")
-    time.sleep(10)
+    time.sleep(5)
 
 time.sleep(180)
