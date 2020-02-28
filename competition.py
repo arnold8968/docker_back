6b1526a236ae599b72393b16a4f829b60c4f0813
James Yan <840520333@qq.com>
2020-02-27 23:36:36 -0500

    Update competition.py

--- competition.py
+++ competition.py
@@ -76,7 +76,7 @@ usage_history = get_cpu()
 for i in usage_history:
     usage_history[i] = [usage_history[i]]
 
-for t in range(25):
+for t in range(30):
     # G = too fast,  B = bad----too slow , S = stay the  same ( balanced)
     G,B,S  = [],[],[]
     Rg,Rb,Qg,Qb = 0,0,0,0
