e9974f3d728a36382915ba1bb3c45becc45a8c59
James Yan <840520333@qq.com>
2020-02-28 09:52:21 -0500

    Update start_container_seven_swarm.py

--- start_container_seven_swarm.py
+++ start_container_seven_swarm.py
@@ -24,13 +24,13 @@ def run_container(container_name,container_model):
     print("Succesfully run container ",container_name,"collecting data now!")
 
 container_list = ["test1","test2","test3","test4","test5","test6","test7"]
-container_model_list = ["fuzzychen/1000batch","fuzzychen/vgg16","fuzzychen/inceptionv3","fuzzychen/res50","fuzzychen/xcep", "fuzzychen/inceptionv3","fuzzychen/res50"]
+container_model_list = ["fuzzychen/1000batch","fuzzychen/vgg16","fuzzychen/inceptionv3","fuzzychen/res50","fuzzychen/xcep"]
 ran_list = random.sample(range(len(container_model_list)), len(container_model_list))
 
 
 for i in range(1, 28):
     run_container('test{}'.format(i), container_model_list[i % len(container_model_list)])
     print("initializing~~~~~~~~~~~~Takes 180 seconds")
-    time.sleep(5)
+    time.sleep(3)
 
 time.sleep(120)
